4517045b76054f588d295fc1b2eed2213a3cdbdf
galina-matyasheva <43518800+galina-matyasheva@users.noreply.github.com>
2020-07-05 19:21:00 +0300

    minor fixes

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser')
 const cors = require('cors')
 
 const db = require('./db')
-const movieRouter = require('./routes/PropertyInfoRouter')
+const auctionRouter = require('./routes/PropertyInfoRouter')
 
 const app = express()
 const apiPort = 3000
@@ -18,7 +18,7 @@ app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
-app.use('/api', movieRouter)
+app.use('/api', auctionRouter)
 
 
 app.listen(apiPort, () => console.log(`Server running on port ${apiPort}`));
